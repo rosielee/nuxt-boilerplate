19004e18fe345143f84afbcd0b6edfeca092f6c8
nickthegit <nick@rosielee.co.uk>
2020-08-19 21:47:25 +0100

    add header, nav socials and hamburgers

---
+++ components/layout/mainHeader.vue
@@ -0,0 +1,77 @@
+<template>
+  <header :class="navStatus ? 'navOpen' : 'navClosed'">
+    <section id="brand" @click="navStatus = false">
+      <nuxt-link to="/">brand here</nuxt-link>
+    </section>
+    <main-nav @click="navStatus = false" id="mainNav" />
+    <button
+      class="hamburger hamburger--collapse"
+      type="button"
+      :class="navStatus ? 'open is-active' : 'closed'"
+      @click="navStatus = !navStatus"
+    >
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+  </header>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        navStatus: false,
+      }
+    },
+    mounted() {
+      // console.log(this.navStatus)
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/hamburgers/hamburgers.scss';
+
+  header {
+    position: relative;
+    z-index: 999999;
+    top: 0;
+    left: 0;
+  }
+  // * desktop style remove if all hamburger
+  button {
+    display: none;
+    position: relative;
+  }
+  // * =======
+  @include breakpoint(mobile) {
+    // * mobile to styles to use as desktop  styles if all hamburger
+    #brand {
+      position: relative;
+      z-index: 9;
+      display: block;
+    }
+    button {
+      display: block;
+      z-index: 10;
+    }
+    #mainNav {
+      position: absolute;
+      width: 100%;
+      height: 100vh;
+      top: 0;
+      left: 0;
+      background: navajowhite;
+      display: none;
+    }
+    // nav open
+    .navOpen {
+      #mainNav {
+        display: block;
+        z-index: 5;
+      }
+    }
+    // * =======
+  }
+</style>
---
+++ components/layout/mainNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <nuxt-link to="/">home</nuxt-link>
+    <nuxt-link to="/a-page">a page</nuxt-link>
+    <nuxt-link to="/dfd">404</nuxt-link>
+    <socials />
+  </nav>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
